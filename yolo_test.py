f95e0a65c73fd4a7f65b1dde691245859026f8c0
puhaiyang <761396462@qq.com>
2026-02-05 14:40:26 +0800

    feat: 添加对测试图像文件的注释，便于后续测试

--- yolo_test.py
+++ yolo_test.py
@@ -3,4 +3,5 @@ from ultralytics import YOLO
 model = YOLO("yolo26n.pt")
 results = model("test_images/1.jpg")
 # results = model("test_images/2.jpg")
+# results = model("test_images/3.jpg")
 results[0].show()
